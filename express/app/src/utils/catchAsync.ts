6bcfa5265dec5ce9c87d7849403198a2269d8ea9
ANPHANVAN <2k1.xam@gmail.com>
2026-01-27 21:07:53 +0700

    feat: enhance error handling by introducing AppCodeError and updating user service and catchAsync utility

--- express/app/src/utils/catchAsync.ts
+++ express/app/src/utils/catchAsync.ts
@@ -1,7 +1,15 @@
+import { EErrorCodes } from '@/constants/errorCode';
+import { AppCodeError, AppError } from '@/middlewares/errorHandler';
 import { Request, Response, NextFunction } from 'express';
 
 export const catchAsync = (fn: any) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch((err: any) => next(err));
+    fn(req, res, next).catch((err: any) => {
+      if (err instanceof AppError) {
+        next(err);
+      } else {
+        next(new AppCodeError(EErrorCodes.INTERNAL_SERVER_ERROR));
+      }
+    });
   };
 };
