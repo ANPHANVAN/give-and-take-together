22a00776042489ab453cc7bdae80c533f186e86f
ANPHANVAN <2k1.xam@gmail.com>
2026-03-05 14:23:15 +0700

    feat: delete module don't need

--- nestjs/app/src/common/filters/filters.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HttpExceptionFilter } from './http-exception/http-exception.filter';
-
-@Module({
-  providers: [HttpExceptionFilter],
-  exports: [HttpExceptionFilter],
-  imports: [],
-})
-export class FiltersModule {}
